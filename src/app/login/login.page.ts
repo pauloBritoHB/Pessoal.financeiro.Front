d030617a1b1874bf827f81ef9f444857ef265ccb
pauloBritoHB <paulo.brito@hbsis.com.br>
2020-10-05 12:00:36 -0300

    começãndo brincar com a tela principal

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -58,4 +58,6 @@ export class LoginPage {
         }
       );
   }
+
+  public cadastrar(): void { }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -11,11 +12,75 @@ export class Tab1Page {
 
   public mensagem = 'pagina1';
 
-  constructor() {
+  constructor(private alertCtrl: AlertController) {
 
   }
 
+  public async depositar() {
+    const alert = await this.alertCtrl.create({
+      header: 'Depositar',
+      message: 'Informar valor de entrada',
+      inputs: [
+        {
+          name: 'quantiadde',
+          placeholder: 'quantidade',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'CANCELA',
+          role: 'calcel',
+          handler: () => { }
+        },
+        {
+          text: 'CONFIRMAR ',
+          role: 'calcel',
+          handler: data => {
+            const valor: any = parseFloat(data.quantiadde);
+            console.log(valor);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
+  public async retirar() {
+    const alert = await this.alertCtrl.create({
+      header: 'Depositar',
+      message: 'Informar valor de retirada',
+      inputs: [
+        {
+          name: 'quantiadde',
+          placeholder: 'quantidade',
+          type: 'number'
+        },
+        {
+          name: 'descricao',
+          placeholder: 'descrição',
+          type: 'text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'CANCELA',
+          role: 'calcel',
+          handler: () => { }
+        },
+        {
+          text: 'CONFIRMAR ',
+          role: 'calcel',
+          handler: data => {
+            const valor: any = parseFloat(data.quantiadde);
+            const descrição: any = data.descricao;
+            console.log(valor, descrição);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 
 
 }
